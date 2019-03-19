fb53bb57b9cdedc1c2eff92c4f48fe02efe3a9c1
Amirhossein Azimyzadeh <39561409+AmirhosseinAzimyzadeh@users.noreply.github.com>
2019-03-19 16:30:36 +0330

    add test to counting sort

--- src/CountingSort.java
+++ src/CountingSort.java
@@ -1,9 +1,18 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+/**
+ * @author : Amirhossein Azimyzadeh
+ * */
 public class CountingSort {
-    public static void main(String[] args) {
-
+    @Test
+    public void test(){
+        int test1[] = new int[]{9,6,4,3,2,1};
+        int test2[] =  new int[]{100,2,1,3,4,5,70};
+        Assertions.assertArrayEquals(new int[]{0,1,2,3,4,6,9},countingSort(test1,9));
+        Assertions.assertArrayEquals(new int[]{0,1,2,3,4,5,70,100},countingSort(test2,100));
     }
 
-    public void countingSort(int[] array, int max){
+    public int[] countingSort(int[] array, int max){
         int[] countingArray = new int[max+1];
         for (int element : array) {
             countingArray[element]++;
@@ -12,7 +21,10 @@ public class CountingSort {
             countingArray[i]+=countingArray[i-1];
         }
         // now counting array ready to use -->
-        int[] result = new int[array.length];
-
+        int[] result = new int[array.length+1];
+        for (int i = array.length-1; i >= 0 ; i--) {
+            result[countingArray[array[i]]]=array[i];
+        }
+        return result;
     }
 }
