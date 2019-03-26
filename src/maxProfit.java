1f1ff9fb9d1e4b02ded21f509ccfe6d2adc74ade
Amirhossein Azimyzadeh <39561409+AmirhosseinAzimyzadeh@users.noreply.github.com>
2019-03-26 18:43:18 +0430

    add max profit file

---
+++ src/maxProfit.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+public class maxProfit {
+
+    @Test
+    public void test(){
+        //TODO
+    }
+
+    public void maxProfit(){
+        //TODO
+    }
+
+}
