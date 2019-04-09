8939d20874da6b91147aae8e39eb687c390a3aac
Amirhossein Azimyzadeh <39561409+AmirhosseinAzimyzadeh@users.noreply.github.com>
2019-04-09 18:25:23 +0430

    rod cutting completed

--- src/RodCutting.java
+++ src/RodCutting.java
@@ -1,3 +1,6 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 /**
  * @author : Amirhossein Azimyzadeh
  * Strategy : Dynamic Programming
@@ -6,9 +9,48 @@
 * recursive formula --> R(n) = max{Price[i] + R (n-1) }
 * */
 public class RodCutting {
-    int[] prices ;
-    public int rodCut(int n){
-        return -1;
-        //TODO
+
+    private int[] R ;
+    private int[] X ; // for printing solution
+
+    public int rodCut(int n , int[] prices){
+        R = new int[n+1]; // indexes start from 1
+        X = new int[n+1];
+        // price of piece with length i is in prices[i]
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= i; j++) {
+                if(R[i]< prices[j]+R[i-j]){
+                    R[i]= prices[j]+R[i-j];
+                    X[i]=j; // best cut place
+                }
+            }
+        return R[n];
+    }
+
+    public String solution(){
+        return "pieces length --> " + solution(R.length-1);
+    }
+
+    private String solution(int n){
+        if(n==0)
+            return " ";
+        return X[n]+" ,"+solution(n-X[n]);
+    }
+    ////////////////////////////////////////////////////////////////////
+    // main for testing solution String
+    public static void main(String[] args) {
+        int[] p = new int[]{0 ,3  ,5  ,8  ,9 ,10 ,17 ,17 ,20};
+        RodCutting rd = new RodCutting();
+        System.out.println(rd.rodCut(8,p));
+        System.out.println(rd.solution());
+    }
+
+    @Test
+    public void test(){
+        int[] p1 = new int[]{0 ,3  ,5  ,8  ,9 ,10 ,17 ,17 ,20};
+        int[] p2 = new int[]{0 ,1  ,5  ,8  ,9 ,10 ,17 ,17 ,20};
+        //test cases from : geeksForGeeks.org
+        Assertions.assertEquals(rodCut(8,p1),24);
+        Assertions.assertEquals(rodCut(8,p2),22);
     }
 }
